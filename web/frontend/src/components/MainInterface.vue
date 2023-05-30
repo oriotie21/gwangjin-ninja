de9ed84c93ae5d1d015ff82c5348b1385d418638
s1lv3rrud <lek8575@naver.com>
2023-05-30 20:47:09 +0900

    interface-table

--- web/frontend/src/components/MainInterface.vue
+++ web/frontend/src/components/MainInterface.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    <p></p>
-    <h1><b>Network Interfaces</b></h1>
+    <div>
+      <p></p>
+    </div>
+    <div class="centered">
+      <h1><b>Network Interfaces</b></h1>
+    </div>
+    <div>
+      <p></p>
+    </div>
+    <div style="display: flex; justify-content: center">
+      <table class="table table-striped table-bordered" style="width: 600px">
+        <thead>
+          <tr>
+            <th style="text-align: center">Interface Name</th>
+            <th style="text-align: center">Use</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style="text-align: center">eth0</td>
+            <td style="text-align: center"><b>O</b></td>
+          </tr>
+          <tr>
+            <td style="text-align: center">lo</td>
+            <td style="text-align: center"><b>X</b></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <!-- <h3 v-for="interfaceName in interfaces" :key="interfaceName">
       <b>- {{ interfaceName }}</b>
     </h3> -->
-    <h2><b> - eth0</b></h2>
-    <h2>- lo</h2>
   </div>
 </template>
 
@@ -36,3 +61,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.centered {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
