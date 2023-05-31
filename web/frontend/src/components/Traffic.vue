ca89c6426f9544ba159f4600486777ec6a800fd3
s1lv3rrud <lek8575@naver.com>
2023-05-31 14:28:23 +0900

    Update Traffic.vue

--- web/frontend/src/components/Traffic.vue
+++ web/frontend/src/components/Traffic.vue
@@ -253,7 +253,7 @@ export default {
                   )
               )
             ) {
-              this.showInternetNotification();
+              //this.showInternetNotification();
             }
           }
         })
